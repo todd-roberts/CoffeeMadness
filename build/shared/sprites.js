f4a30e8743be5953b38f1da36a25d91a9041a315
Todd Roberts <todd@system76-pc.localdomain>
2019-05-06 04:02:37 -0500

    added stuff

--- build/shared/sprites.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.sprites = void 0;
-var sprites = [null, 'platform', 'cloud', 'coffee', 'breakroom_coffee', 'monster', 'carafe'];
-exports.sprites = sprites;
